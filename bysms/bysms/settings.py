001c96bb6a6763bae4e445d10d2571a5e981737e
ArtistVan1 <ytjunjie2006@126.com>
2020-10-07 17:18:13 +0900

    DataBase_filter

--- bysms/bysms/settings.py
+++ bysms/bysms/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     #new
     'common.apps.CommonConfig',
     'boys.apps.BoysConfig'
--- bysms/sales/urls.py
+++ bysms/sales/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from sales.views import listorders,listorders1,listorders2,listorders3
+from sales.views import listorders,listorders1,listorders2,listorders3,listcustomers
 
 urlpatterns = [
 
     path('orders/',listorders),
     path('orders1/',listorders1),
     path('orders2/',listorders2),
-    path('orders3/',listorders3)
+    path('orders3/',listorders3),
+    path('customers/',listcustomers)
 
 ]
